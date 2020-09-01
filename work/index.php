c4eaa1d37250bcec14026355c2d53e10254d287b
根本翔伍 <shota0701nemoto@gmail.com>
2020-09-01 18:51:39 +0900

    課題2

--- work/index.php
+++ work/index.php
@@ -1,12 +1,25 @@
 <?php
-echo "PHP<br/>";
-echo "curriculum<br/>";
+ echo "PHP<br/>";
+ echo "curriculum<br/>";
 ?>
 
 <?php
-echo 7 + 1;
-echo 10 - 10;
-echo 10 * 10;
-echo 10 / 10;
+ echo 7 + 1;
+ echo 10 - 10;
+ echo 10 * 10;
+ echo 10 / 10;
 ?>
+
+
+<?php
+ $name = "Hello, World!<br/>";
+ echo $name;
+ $number = 1;
+ $number_2 = 2;
+ echo $number + $number_2;
+ echo $number - $number_2;
+ echo $number * $number_2;
+ echo $number / $number_2;
+?>
+
 
