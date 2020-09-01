5522e2b68114b040f12b9a66ad341bdda6e0695a
根本翔伍 <shota0701nemoto@gmail.com>
2020-09-01 20:19:48 +0900

    課題5

--- work/index.php
+++ work/index.php
@@ -47,12 +47,50 @@
 <?php
  $name1 = "根本";
  $name2 = "翔伍";
-echo $name1 . $name2;
-$name1 .= "PHP";
-echo $name1 . $name2;
+ echo $name1 . $name2;
+ $name1 .= "PHP";
+ echo $name1 . $name2;
  ?>
 
+<?php
+ echo "こんにちは！ \"'今' 私は’PHP’を勉強しています";
+?>
 
 <?php
-echo "こんにちは！ \"'今' 私は’PHP’を勉強しています";
+ $num = rand(1,10);
+   if ($num / 2 ) {
+   echo '偶数';
+ } else {
+   echo '奇数';
+   }
+ echo $num;
+?>
+
+<?php
+ $score = rand(0,100);
+   if ($score > 0 && $score < 49 ){
+    echo '不可';
+ } else if ($score > 50 && $score < 69 ){
+    echo '可';    
+ } else if ($score > 70 && $score < 79 ){
+    echo '良';    
+ } else if ($score > 80 && $score < 99 ){
+    echo '優';    
+ } else {
+    echo '満点';    
+ }
+?>
+
+<?php
+ $score1 = rand(0,100);
+ $score2 = rand(0,100);
+   if ($score1 > 60 && $score2 < 60 ){
+    echo '合格';
+ } else if ($score1 + $score2 > 130 ){
+    echo '合格';    
+ } else if ($score1 + $score2 > 100 && (score1 > 90 or score2 > 90)  ){
+    echo '合格';    
+ } else {
+    echo '不合格';    
+ } 
 ?>
