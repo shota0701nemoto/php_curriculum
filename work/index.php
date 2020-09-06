b639a52b4c36c53689837612e6be9cbcb718cbc1
根本翔伍 <shota0701nemoto@gmail.com>
2020-09-06 19:59:14 +0900

    課題11

--- work/index.php
+++ work/index.php
@@ -342,3 +342,25 @@ foreach ($data[0] as $key => $value) {
   var_dump($object);
 ?>
 </pre>
+
+<?php
+  $message = htmlspecialchars($_GET['message'], ENT_QUOTES, 'UTF-8');
+  echo $message;
+  print_r($_GET);
+
+?>
+
+<!DOCTYPE html>
+<html lang = "ja">
+<head><meta charset = "UFT-8"></head>
+<body>
+<form action = "index.php" method = "get">
+<input type = "text" name ="message"><br/>
+<input type="checkbox" name="hoby[]" value="musicappreciation">音楽鑑賞<br/>
+<input type="checkbox" name="hoby[]" value="moviegoing">映画鑑賞<br/>
+<input type="checkbox" name="hoby[]" value="reading">読書<br/>
+<input type="checkbox" name="hoby[]" value="fishing">釣り<br/>
+<input type = "submit" value ="submit">
+</form>
+</body>
+</html>
